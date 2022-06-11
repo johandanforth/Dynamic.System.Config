370f59e74284c75013144a8cd0cbd8ee26a43eb0
Johan Danforth <johan_danforth@hotmail.com>
2022-06-11 18:18:21 +0200

    Add project files.

---
+++ System.AppSettings.DynamicConfig.Tests/Program.cs
@@ -0,0 +1,47 @@
+﻿// ReSharper disable CheckNamespace
+using System.Diagnostics;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+// implicit conversions from dynamic to string/int/...
+string stringValue = AppSettings<string>.Get.StringValue;
+int intValue = AppSettings<int>.Get.IntValue;
+Guid guidValue = AppSettings<Guid>.Get.GuidValue;
+DateTime dateValue = AppSettings<DateTime>.Get.DateValue;
+// cast
+var dateVal = (DateTime)AppSettings<DateTime>.Get.DateValue;
+var intVal = (int)AppSettings<int>.Get.IntValue;
+
+Console.WriteLine($"string val: {stringValue}");
+Console.WriteLine($"int val: {intValue}");
+Console.WriteLine($"guid val: {guidValue}");
+Console.WriteLine($"date val: {dateValue.ToShortDateString()}");
+
+const int iterations = 10000;
+
+Console.WriteLine($"Benchmarking get integer over {iterations} iterations:");
+
+//benchmarking
+var sw = new Stopwatch();
+sw.Start();
+for (var i = 0; i < iterations; i++)
+{
+	int val = AppSettings<int>.Get.IntVal;
+}
+
+sw.Stop();
+Console.WriteLine($"\tDynamic config: {sw.ElapsedMilliseconds} ms / {sw.ElapsedTicks} ticks");
+
+var config = Host.CreateDefaultBuilder().Build().Services.GetRequiredService<IConfiguration>();
+
+sw.Reset();
+sw.Start();
+for (var i = 0; i < iterations; i++)
+{
+	var val = config.GetValue<int>("IntVal");
+}
+
+sw.Stop();
+Console.WriteLine($"\tIConfiguration: {sw.ElapsedMilliseconds} ms / {sw.ElapsedTicks} ticks");
---
+++ System.AppSettings.DynamicConfig/DynamicConfig.cs
@@ -0,0 +1,31 @@
+﻿// ReSharper disable CheckNamespace
+using System.Dynamic;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace System
+{
+	public static class AppSettings<TReturn>
+	{
+		public static dynamic Get = new DynamicConfig<TReturn>();
+
+		private class DynamicConfig<TValue> : DynamicObject
+		{
+			private readonly IConfiguration _config;
+
+			public DynamicConfig()
+			{
+				_config = Host.CreateDefaultBuilder().Build().Services
+					.GetRequiredService<IConfiguration>();
+			}
+
+			public override bool TryGetMember(GetMemberBinder binder, out object result)
+			{
+				result = _config.GetValue<TValue>(binder.Name);
+				return true;
+			}
+		}
+	}
+}
