a4ef78925903380b04168404a03b130a594aa66a
Johan Danforth <johan_danforth@hotmail.com>
2022-06-12 16:56:22 +0200

    unit test tweaks

--- System.AppSettings.DynamicConfig.Tests/Tests.cs
+++ System.AppSettings.DynamicConfig.Tests/Tests.cs
@@ -1,51 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Xunit;
+﻿using Xunit;
 using Xunit.Abstractions;
 
-namespace System.AppSettings.DynamicConfig.Tests
+namespace System.AppSettings.DynamicConfig.Tests;
+
+public class Tests
 {
-    public class Tests
-    {
-	    private readonly ITestOutputHelper _testOutputHelper;
-	    public Tests(ITestOutputHelper testOutputHelper)
-	    {
-		    _testOutputHelper = testOutputHelper;
-	    }
+	private ITestOutputHelper Console { get; }
 
-	    [Fact]
-	    public void ReadValues()
-	    {
-		    // implicit conversions from dynamic to string/int/...
-		    string stringValue = AppSettings<string>.Get.StringValue;
-		    int intValue = AppSettings<int>.Get.IntValue;
-		    Guid guidValue = AppSettings<Guid>.Get.GuidValue;
-		    DateTime dateValue = AppSettings<DateTime>.Get.DateValue;
+	public Tests(ITestOutputHelper testOutputHelper)
+	{
+		Console = testOutputHelper;
+	}
 
-		    _testOutputHelper.WriteLine($"string val: {stringValue}");
-		    _testOutputHelper.WriteLine($"int val: {intValue}");
-		    _testOutputHelper.WriteLine($"guid val: {guidValue}");
-		    _testOutputHelper.WriteLine($"date val: {dateValue.ToShortDateString()}");
+	[Fact]
+	public void ReadValues()
+	{
+		// implicit conversions from dynamic to string/int/...
+		string stringValue = AppSettings<string>.Get.StringValue;
+		int intValue = AppSettings<int>.Get.IntValue;
+		Guid guidValue = AppSettings<Guid>.Get.GuidValue;
+		DateTime dateValue = AppSettings<DateTime>.Get.DateValue;
 
-			Assert.Equal("The string",stringValue);
-			Assert.Equal(42,intValue);
-			Assert.Equal("C92A574B-98E8-4371-A193-C86F601433DC", guidValue.ToString().ToUpperInvariant());
-			Assert.Equal(new DateTime(2022,6,11), dateValue);
+		Console.WriteLine($"string val: {stringValue}");
+		Console.WriteLine($"int val: {intValue}");
+		Console.WriteLine($"guid val: {guidValue}");
+		Console.WriteLine($"date val: {dateValue.ToShortDateString()}");
 
-            // cast
-            var dateVal = (DateTime)AppSettings<DateTime>.Get.DateValue;
-		    var intVal = (int)AppSettings<int>.Get.IntValue;
+		Assert.Equal("The string", stringValue);
+		Assert.Equal(42, intValue);
+		Assert.Equal("C92A574B-98E8-4371-A193-C86F601433DC",
+			guidValue.ToString().ToUpperInvariant());
 
-		    _testOutputHelper.WriteLine($"int val: {intVal}");
-		    _testOutputHelper.WriteLine($"date val: {dateVal.ToShortDateString()}");
+		Assert.Equal(new DateTime(2022, 6, 11), dateValue);
 
-		    Assert.Equal(42, intVal);
-		    Assert.Equal(new DateTime(2022, 6, 11), dateVal);
+		// cast
+		var dateVal = (DateTime)AppSettings<DateTime>.Get.DateValue;
+		var intVal = (int)AppSettings<int>.Get.IntValue;
 
-        }
-    }
+		Console.WriteLine($"int val: {intVal}");
+		Console.WriteLine($"date val: {dateVal.ToShortDateString()}");
+
+		Assert.Equal(42, intVal);
+		Assert.Equal(new DateTime(2022, 6, 11), dateVal);
+	}
 }
